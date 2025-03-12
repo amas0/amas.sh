687e998d0a816c7dd1a62062fb131ba71664882b
zeevo <8338013+zeevo@users.noreply.github.com>
2025-03-11 23:11:05 -0400

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="space-y-4 flex-grow flex flex-col justify-center">
+      <p className="text-center mt-4 text-foreground">404 Page Not Found</p>
+    </main>
+  );
+}
